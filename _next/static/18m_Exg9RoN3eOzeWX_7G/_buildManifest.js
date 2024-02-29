4aeb650838a7d1cda06a73958b7e6fb794c62220
cherepii <98636205+cherepii@users.noreply.github.com>
2024-02-29 14:27:35 +0600

    Add files via upload

---
+++ _next/static/18m_Exg9RoN3eOzeWX_7G/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/780-91a805c478a03ecc.js","static/chunks/pages/index-cb06afa7f0fd83c6.js"],"/_error":["static/chunks/pages/_error-65ac7445167ddb62.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
